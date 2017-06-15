c0c21cea92fe03a6acf2a9f78d7d80e693608a10
Luz Cazares <luz.cazares@intel.com>
2017-06-15 18:56:58 +0000

    Add script to clone common openstack repos

---
+++ clone_git_repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Clone my common OpenStack projects
+git clone https://github.com/openstack/interop.git
+git clone https://github.com/openstack/refstack.git
+git clone https://github.com/openstack/refstack-client.git
+git clone https://github.com/openstack/tempest.git
+git clone https://github.com/openstack/openstack-ansible.git
+git clone https://github.com/openstack/openstack-ansible-os_tempest.git
+git clone https://github.com/openstack-infra/project-config.git
+git clone https://github.com/dlux/InstallScripts.git
