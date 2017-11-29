5b4f673b3243f535a668a5480bb76107fc183c53
Luz Cazares <luz.cazares@intel.com>
2017-11-29 15:49:06 -0800

    Add umask to devtools script
    Adding 022 as umask at /home/$user/.profile to avoid execution issues
    with default umask.
    See https://askubuntu.com/questions/44542/what-is-umask-and-how-does-it-work

--- devtoolsTMP.sh
+++ devtoolsTMP.sh
@@ -20,12 +20,18 @@ source common_functions
 EnsureRoot
 SetLocale /root
 
+PY3=False
+
 # ======================= Processes installation options =====================
 # Handle file sent as parameter - from where proxy info will be retrieved.
 while [[ ${1} ]]; do
   case "${1}" in
     --help|-h)
-      PrintHelp "Install devtools" $(basename "$0")
+      PrintHelp "Install devtools" $(basename "$0") \
+          "     --py3             To setup Python3."
+      ;;
+    --py3)
+      PY3=True
       ;;
     *)
       HandleOptions "$@"
@@ -36,8 +42,7 @@ done
 
 # ============================================================================
 # BEGIN PACKAGE INSTALATATION
-
-# If proxy passed as parameter - set it on the VENV
+# If proxy passed as parameter - set it on the environment
 [[ -n $_PROXY ]] && source ".PROXY"
 
 apt-get -y update
@@ -45,7 +50,16 @@ apt-get -y update
 # Setup user calling this script (non-root)
 caller_user=$(who -m | awk '{print $1;}')
 if [ -z $caller_user ]; then
-  [[ $(IsTrusty) == True ]] && caller_user='vagrant' || caller_user='ubuntu'
+    [[ $(IsTrusty) == True ]] && caller_user='vagrant' || caller_user='ubuntu'
+fi
+
+# Set umask to 022
+if [ -f /home/$caller_user/.profile ]; then
+    sed -i 's/#umask/umask/g' /home/$caller_user/.profile
+else
+    cp /etc/skel/.profile /home/$caller_user/.profile
+    sed -i 's/#umask/umask/g' /home/$caller_user/.profile
+    chown $caller_user:$caller_user /home/$caller_user/.profile
 fi
 
 apt-get install -y curl git
@@ -54,14 +68,14 @@ apt-get install -y build-essential libssl-dev libffi-dev libxml2-dev \
 
 #curl -Lo- https://bootstrap.pypa.io/get-pip.py | python
 apt-get install -y python-pip
-sudo -H -u $caller_user bash -c 'pip install --upgrade pip'
-sudo -H -u $caller_user bash -c 'pip install git-review virtualenv'
+pip install --upgrade pip
+pip install git-review virtualenv
 
-if [ $(IsXenial) == True ]; then
-  echo 'Setting up PY3'
-  apt-get install -y python3-dev python3-pip
-  sudo -H -u $caller_user bash -c 'pip3 install --upgrade pip3'
-  sudo -H -u $caller_user bash -c 'pip3 install git-review virtualenv'
+if [[ $(IsXenial) == True && $PY3 == True ]]; then
+    echo 'Setting up PY3'
+    apt-get install -y python3-dev python3-pip
+    pip3 install --upgrade pip3
+    pip3 install git-review virtualenv
 fi
 
 if [ ! -f "/home/$caller_user/.bashrc" ]; then
@@ -78,11 +92,13 @@ sudo -H -u $caller_user bash -c "$cmd gitreview.username 'luzcazares'"
 
 # Bypass proxy on ssh (used by git) via .ssh/config file.
 if [[ ! -z "${_ORIGINAL_PROXY}" ]]; then
-  prx=$(echo "${_ORIGINAL_PROXY}" | awk -F '//' '{print $2}' | 
-        awk -F ':' '{print $1}')
-  cfgFile="/home/$caller_user/.ssh/config"
-  echo "Host *" >> $cfgFile
-  echo "ProxyCommand nc -X 5 -x $prx:1080 %h %p" >> $cfgFile
+    echo 'Setting proxy on .ssh/config'
+    pxSvr=$(echo "${_ORIGINAL_PROXY}" | awk -F '//' '{print $2}' \
+            | awk -F ':' '{print $1}')
+    cfgFile="/home/$caller_user/.ssh/config"
+    echo "Host *" >> "${cfgFile}"
+    echo "ProxyCommand nc -X 5 -x $pxSvr:1080 %h %p" >> "${cfgFile}"
+    chown $caller_user:$caller_user "${cfgFile}"
 fi
 
 # Cleanup _proxy from apt if added - first coincedence
