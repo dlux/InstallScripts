875636be9f243afaa33c5dc1ec5b56f7eba6058e
Luz Cazares <luz.cazares@intel.com>
2017-11-28 17:35:52 -0800

    Adding new version of devtools installation
    Uploaded as TMP until fully tested in order to replace
    install_devtools.sh
    
    This will be a simplified version (not installing virtualenvwrapper)

---
+++ devtoolsTMP.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# ============================================================================
+# Script installs dev tools:
+# pip, git, git-review, virtualenv, build-essential among others
+# ============================================================================
+
+# Uncomment the following line to debug this script
+set -o xtrace
+
+#=============================================================================
+# GLOBAL FUNCTIONS
+#=============================================================================
+
+[[ ! -f common_functions ]] && curl -O \
+  https://raw.githubusercontent.com/dlux/InstallScripts/master/common_functions
+[[ ! -f common_functions ]] && exit 1
+source common_functions
+
+EnsureRoot
+SetLocale /root
+
+# ======================= Processes installation options =====================
+# Handle file sent as parameter - from where proxy info will be retrieved.
+while [[ ${1} ]]; do
+  case "${1}" in
+    --help|-h)
+      PrintHelp "Install devtools" $(basename "$0")
+      ;;
+    *)
+      HandleOptions "$@"
+      shift
+  esac
+  shift
+done
+
+# ============================================================================
+# BEGIN PACKAGE INSTALATATION
+
+# If proxy passed as parameter - set it on the VENV
+[[ -n $_PROXY ]] && source ".PROXY"
+
+apt-get -y update
+
+# Setup user calling this script (non-root)
+caller_user=$(who -m | awk '{print $1;}')
+if [ -z $caller_user ]; then
+  [[ $(IsTrusty) == True ]] && caller_user='vagrant' || caller_user='ubuntu'
+fi
+
+apt-get install -y curl git
+apt-get install -y build-essential libssl-dev libffi-dev libxml2-dev \
+                   libxslt1-dev libpq-dev python-dev
+
+#curl -Lo- https://bootstrap.pypa.io/get-pip.py | python
+apt-get install -y python-pip
+sudo -H -u $caller_user bash -c 'pip install --upgrade pip'
+sudo -H -u $caller_user bash -c 'pip install git-review virtualenv'
+
+if [ $(IsXenial) == True ]; then
+  echo 'Setting up PY3'
+  apt-get install -y python3-dev python3-pip
+  sudo -H -u $caller_user bash -c 'pip3 install --upgrade pip3'
+  sudo -H -u $caller_user bash -c 'pip3 install git-review virtualenv'
+fi
+
+if [ ! -f "/home/$caller_user/.bashrc" ]; then
+    cp /etc/skel/.bashrc "/home/$caller_user/.bashrc"
+    chown $caller_user:$caller_user "/home/$caller_user/.bashrc"
+fi
+
+echo 'Configuring git & git-review'
+cmd='git config --global'
+sudo -H -u $caller_user bash -c "$cmd user.name 'Luz Cazares'"
+sudo -H -u $caller_user bash -c "$cmd user.email 'luz.cazares@intel.com'"
+sudo -H -u $caller_user bash -c "$cmd core.editor 'vim'"
+sudo -H -u $caller_user bash -c "$cmd gitreview.username 'luzcazares'"
+
+# Bypass proxy on ssh (used by git) via .ssh/config file.
+if [[ ! -z "${_ORIGINAL_PROXY}" ]]; then
+  prx=$(echo "${_ORIGINAL_PROXY}" | awk -F '//' '{print $2}' | 
+        awk -F ':' '{print $1}')
+  cfgFile="/home/$caller_user/.ssh/config"
+  echo "Host *" >> $cfgFile
+  echo "ProxyCommand nc -X 5 -x $prx:1080 %h %p" >> $cfgFile
+fi
+
+# Cleanup _proxy from apt if added - first coincedence
+UnsetProxy
+
