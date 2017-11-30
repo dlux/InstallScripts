8d4c924d0f79ee6300798361340782a12702b574
Luz Cazares <luz.cazares@intel.com>
2017-11-30 15:29:19 -0800

    Replace install_devtools script with new version

--- install_devtools.sh
+++ install_devtools.sh
@@ -2,29 +2,37 @@
 
 # ============================================================================
 # Script installs dev tools:
-# pip, git, git-review, virtualenv, virtualenvwrapper,
-# build-essential among others
+# pip, git, git-review, virtualenv, build-essential among others
 # ============================================================================
 
 # Uncomment the following line to debug this script
-# set -o xtrace
+set -o xtrace
 
 #=============================================================================
 # GLOBAL FUNCTIONS
 #=============================================================================
 
-[[ ! -f common_functions ]] && curl -O https://raw.githubusercontent.com/dlux/InstallScripts/master/common_functions
+[[ ! -f common_functions ]] && curl -O \
+  https://raw.githubusercontent.com/dlux/InstallScripts/master/common_functions
+[[ ! -f common_functions ]] && exit 1
 source common_functions
 
 EnsureRoot
 SetLocale /root
+umask 022
+
+PY3=False
 
 # ======================= Processes installation options =====================
 # Handle file sent as parameter - from where proxy info will be retrieved.
 while [[ ${1} ]]; do
   case "${1}" in
     --help|-h)
-      PrintHelp "Install devtools" $(basename "$0")
+      PrintHelp "Install devtools" $(basename "$0") \
+          "     --py3             To setup Python3."
+      ;;
+    --py3)
+      PY3=True
       ;;
     *)
       HandleOptions "$@"
@@ -35,65 +43,56 @@ done
 
 # ============================================================================
 # BEGIN PACKAGE INSTALATATION
-eval $_PROXY apt-get -y update
-eval $_PROXY apt-get install -y curl git
-eval $_PROXY apt-get install -y build-essential libssl-dev libffi-dev \
-python-dev libxml2-dev libxslt1-dev libpq-dev
-eval $_PROXY curl -Lo- https://bootstrap.pypa.io/get-pip.py | eval $_PROXY python
-eval $_PROXY pip install git-review
-eval $_PROXY pip install virtualenv
-eval $_PROXY pip install virtualenvwrapper
+# If proxy passed as parameter - set it on the environment
+[[ -n $_PROXY ]] && source ".PROXY"
 
-# Install py3 if it is xenial
-# TODO in future remove support for trusty and prepare for xenial and up
-if [[ $(IsXenial) == True ]]; then
-  eval $_PROXY apt-get install -y python3-dev
-  # Fix pip and virtualenv pyhton versions
-  eval $_PROXY curl -Lo- https://bootstrap.pypa.io/get-pip.py | eval $_PROXY python3 -v
-  eval $_PROXY pip install --upgrade virtualenv
-fi
+apt-get -y update
 
 # Setup user calling this script (non-root)
 caller_user=$(who -m | awk '{print $1;}')
 if [ -z $caller_user ]; then
-  [[ $(IsTrusty) == True ]] && caller_user='vagrant' || caller_user='ubuntu'
-fi
-caller_home="/home/$caller_user"
-echo $caller_home
-
-if [ ! -d "$caller_home/.virtualenvs" ]; then
-    mkdir "$caller_home/.virtualenvs"
-    chown $caller_user:$caller_user "$caller_home/.virtualenvs"
+    [[ $(IsTrusty) == True ]] && caller_user='vagrant' || caller_user='ubuntu'
 fi
 
-if [ ! -f "$caller_home/.bashrc" ]; then
-    cp /etc/skel/.bashrc "$caller_home/.bashrc"
-    chown $caller_user:$caller_user "$caller_home/.bashrc"
+apt-get install -y curl git
+apt-get install -y build-essential libssl-dev libffi-dev libxml2-dev \
+                   libxslt1-dev libpq-dev python-dev
+
+#curl -Lo- https://bootstrap.pypa.io/get-pip.py | python
+apt-get install -y python-pip
+pip install --upgrade pip
+pip install git-review virtualenv
+
+if [[ $(IsXenial) == True && $PY3 == True ]]; then
+    echo 'Setting up PY3'
+    apt-get install -y python3-dev python3-pip
+    pip3 install --upgrade pip3
+    pip3 install git-review virtualenv
 fi
 
-cat <<EOF >> "$caller_home/.bashrc"
-export WORKON_HOME=$caller_home/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-EOF
+if [ ! -f "/home/$caller_user/.bashrc" ]; then
+    cp /etc/skel/.bashrc "/home/$caller_user/.bashrc"
+    chown $caller_user:$caller_user "/home/$caller_user/.bashrc"
+fi
 
-# Configure git & git-review
-#-----------------------------
-sudo -H -u $caller_user bash -c 'git config --global user.name "Luz Cazares"'
-sudo -H -u $caller_user bash -c 'git config --global user.email "luz.cazares"'
-sudo -H -u $caller_user bash -c 'git config --global gitreview.username "luzcazares"'
+echo 'Configuring git & git-review'
+cmd='git config --global'
+sudo -H -u $caller_user bash -c "$cmd user.name 'Luz Cazares'"
+sudo -H -u $caller_user bash -c "$cmd user.email 'luz.cazares@intel.com'"
+sudo -H -u $caller_user bash -c "$cmd core.editor 'vim'"
+sudo -H -u $caller_user bash -c "$cmd gitreview.username 'luzcazares'"
 
-# If behind proxy create .ssh/config file to bypass proxy
+# Bypass proxy on ssh (used by git) via .ssh/config file.
 if [[ ! -z "${_ORIGINAL_PROXY}" ]]; then
-  prx=$(echo "${_ORIGINAL_PROXY}" | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
-  echo "Host *" >> "$caller_home"/.ssh/config
-  echo "ProxyCommand nc -X 5 -x $prx:1080 %h %p" >> "$caller_home"/.ssh/config
+    echo 'Setting proxy on .ssh/config'
+    pxSvr=$(echo "${_ORIGINAL_PROXY}" | awk -F '//' '{print $2}' \
+            | awk -F ':' '{print $1}')
+    cfgFile="/home/$caller_user/.ssh/config"
+    mkdir -p "/home/$caller_user/.ssh"
+    echo "Host *" >> "${cfgFile}"
+    echo "ProxyCommand nc -X 5 -x $pxSvr:1080 %h %p" >> "${cfgFile}"
+    chown $caller_user:$caller_user "${cfgFile}"
 fi
-# When using proxy, other method is to use http instead of git/ssh for the connection
-#if [[ ! -z $_PROXY ]]; then
-#    git config --global url.https://.insteadOf git://
-#    git config --global gitreview.scheme https
-#    git config --global gitreview.port 443
-#fi
 
 # Cleanup _proxy from apt if added - first coincedence
 UnsetProxy
