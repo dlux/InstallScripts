fb8f740658d6420d2cd9cc982b26c9803c8b33f0
Ubuntu <ubuntu@general>
2017-12-22 20:09:52 +0000

    Cobbler script ammendments
    Still WIP

--- install_cobbler.sh
+++ install_cobbler.sh
@@ -18,12 +18,15 @@ source common_packages
 EnsureRoot
 SetLocale /root
 _PASSWORD='secure123'
-
 # ========================= Processes installation options ===================
 while [[ ${1} ]]; do
   case "${1}" in
     --help|-h)
-      PrintHelp "Install cobbler" $(basename "$0")
+      read -d '' opts <<- EOF
+\      --password | -p  Cobbler password.
+EOF
+      PrintHelp "Install cobbler" $(basename "$0") \
+      ""
       ;;
     --password|-p)
       [[ -z "${2}" ]] && PrintError "Must provide a password."
@@ -40,41 +43,49 @@ done
 # ============================= Cobbler instalation ==========================
 [[ -n $http_proxy ]] && SetProxy $http_proxy
 
+echo '-->  Install prereqs'
 apt-get -y -qq update
-apt-get -y install python
-./install_devtools.sh
+InstallDhcp
 
-InstallApache
-CustomizeApache
-InstallApacheMod wsgi
-InstallApacheMod ssl
-InstallApacheMod fqdn
-apt-get -y install createrepo mkisofs python-cheetah python-netaddr \
-    python-simplejson python-urlgrabber python-yaml rsync syslinux \
-    python-django python-setuptools openssl
+echo '-->  Ensure SELinux is disabled or in permissive mode'
+sudo apt-get install -y policycoreutils
+[[ -n $(sestatus | grep enabled) ]] && setenforce 0
 
-# TFTP
-InstallTftp
-apt-get -y install atftpd
-ln -s /srv/tftp /var/lib/tftpboot
-
-# Cobbler
-apt-get -y install cobbler cobbler-web
-chown www-data /var/lib/cobbler/webui_sessions
+echo '-->  Installing cobbler and dependencies'.
+apt-get install -y cobbler cobbler-web
 # Fix issue with apache2 mod_python by dissabling it
 a2dismod python
 systemctl restart apache2
-systemctl status apache2
+chown www-data /var/lib/cobbler/webui_sessions
 
-echo "Changing cobbler encrypted password"
+echo 'CONFIGURE COBBLER:'
+echo '-->  Changing cobbler encrypted password'
 echo "$_PASSWORD" > ~/.cryptedPassword
 crypt_pass=$(openssl passwd -1 -in ~/.cryptedPassword)
 echo "$crypt_pass" > ~/.cryptedPassword
-sed -i "s/^default_password_crypted.*$/default_password_crypted: \"$crypt_pass\"/g"/etc/cobbler/settings
-# sed -i "s/^server:.*/server: $IP/g" /etc/cobbler/settings
+sed -i "s/^default_password_crypted.*$/default_password_crypted: \"$crypt_pass\"/g" /etc/cobbler/settings
 
-echo "Configuring DHCP Management"
+echo '-->  Setting Server IPs'
+HOST_IP=$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')
+if [[ -z $(cat /etc/cobbler/settings | grep -e "^server:.*$HOST_IP") ]]; then
+    sed -i "s/^server:.*/server: $HOST_IP/g" /etc/cobbler/settings
+    sed -i "s/^next_server:.*/next_server: $HOST_IP/g" /etc/cobbler/settings
+fi
+
+echo '-->  Configuring DHCP Management'
 sed -i "s/^manage_dhcp.*/manage_dhcp: 1/g" /etc/cobbler/settings
+# Modify DHCP net/ ip range
+# Use default 192.168.1.0/24 (max 254 hosts)
+ConfigDhcpapt
+
+# Change data dir from /var/www to /opt/cobbler
+# See http://cobbler.github.io/manuals/2.6.0/2/5_-_Relocating_Your_Installation.html
+exit 0
+cobbler sync
+cobbler get-loaders
+echo '-->  Starting cobbler.'
+systemctl enable cobblerd.service
+systemctl start cobblerd.service
 
 # Cleanup _proxy from apt if added - first coincedence
 UnsetProxy $_ORIGINAL_PROXY
